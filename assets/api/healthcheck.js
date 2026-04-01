aa9e784612bdead62e83dd3ef2bdf05ee57e989b
dnaccarato <dnaccarato@captechventures.com>
2026-04-01 14:23:48 -0400

    Test api -> db access

--- assets/api/healthcheck.js
+++ assets/api/healthcheck.js
@@ -6,3 +6,12 @@ document.getElementById("healthCheckBtn").addEventListener("click", async () => 
     console.log(data);
     alert("API status: " + data.status);
 });
+
+document.getElementById("playersBtn").addEventListener("click", async () => {
+    const res = await fetch(API_BASE_URL+ "/players");
+    const data = await res.json();
+    console.log(data);
+    alert("Players: " + data);
+});
+
+// TODO call the health check endpoint when someone opens the site and every 10 minutes to avoid cold starts
